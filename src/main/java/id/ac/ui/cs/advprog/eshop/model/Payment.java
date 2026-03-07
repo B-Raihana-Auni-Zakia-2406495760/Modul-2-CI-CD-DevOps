80c5e1f48f0ba0487b0ce8d6ed61624892e05780
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-03-07 21:33:49 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -16,6 +16,29 @@ public class Payment {
     private Order order;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+
+        if ("VOUCHER".equals(method)) {
+            String code = paymentData.get("voucherCode");
+            if (code != null && code.length() == 16 && code.startsWith("ESHOP") && code.chars().filter(Character::isDigit).count() == 8) {
+                this.status = "SUCCESS";
+            } else {
+                this.status = "REJECTED";
+            }
+        } else if ("BANK".equals(method)) {
+            String bankName = paymentData.get("bankName");
+            String refCode = paymentData.get("referenceCode");
+            if (bankName != null && !bankName.trim().isEmpty() && refCode != null && !refCode.trim().isEmpty()) {
+                this.status = "SUCCESS";
+            } else {
+                this.status = "REJECTED";
+            }
+        } else {
+            this.status = "REJECTED";
+        }
     }
 
 }
