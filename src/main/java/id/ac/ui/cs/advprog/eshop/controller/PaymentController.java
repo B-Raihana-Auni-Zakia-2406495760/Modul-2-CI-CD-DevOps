0b6e75e5a9aaa210a3fb9cb873bc7f64b085ab34
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-03-08 00:20:16 +0700

    [GREEN] Implement PaymentController class

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/PaymentController.java
@@ -5,24 +5,28 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/payment")
 public class PaymentController {
-
+    @GetMapping("/detail")
     public String paymentDetail() {
-        return null;
+        return "payment/detail";
     }
 
+    @GetMapping("/detail/{paymentId}")
     public String paymentDetailById(@PathVariable String paymentId) {
-        return null;
+        return "payment/detail";
     }
 
+    @GetMapping("/admin/list")
     public String paymentAdminList() {
-        return null;
+        return "payment/admin/list";
     }
 
+    @GetMapping("/admin/detail/{paymentId}")
     public String paymentAdminDetail(@PathVariable String paymentId) {
-        return null;
+        return "payment/admin/detail";
     }
 
+    @PostMapping("/admin/set-status/{paymentId}")
     public String paymentSetStatus(@PathVariable String paymentId) {
-        return null;
+        return "payment/admin/detail";
     }
 }
