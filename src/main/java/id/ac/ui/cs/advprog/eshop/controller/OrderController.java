e95ba94c59caf11959c950e01d53044094329ddc
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-03-08 00:01:32 +0700

    [GREEN] Implement OrderController class

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -5,24 +5,28 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/order")
 public class OrderController {
-
+    @GetMapping("/create")
     public String createOrder() {
-        return null;
+        return "order/create";
     }
 
+    @GetMapping("/history")
     public String historyOrder() {
-        return null;
+        return "order/history";
     }
 
+    @PostMapping("/history")
     public String historyOrderPost() {
-        return null;
+        return "order/history";
     }
 
+    @GetMapping("/pay/{orderId}")
     public String payOrder(@PathVariable String orderId) {
-        return null;
+        return "order/pay";
     }
 
+    @PostMapping("/pay/{orderId}")
     public String payOrderPost(@PathVariable String orderId) {
-        return null;
+        return "order/pay";
     }
 }
